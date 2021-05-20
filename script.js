beddda67ad4a9b08ff024c837f368a1f74ee3420
brunojustino <brunojustino@yahoo.com.br>
2021-05-20 14:53:44 -0300

    added basic functionality, missing the block size option and reset button

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("hello")
+let containerDiv = document.querySelector(".container");
+let root = document.querySelector(':root');
+
+// document.documentElement.style.setProprety("--rowNum", 6) change css variable
+
+function createDiv(){
+    let createdDiv = document.createElement("div");
+    createdDiv.classList.add("createdDiv");
+    containerDiv.appendChild(createdDiv);
+    createdDiv.addEventListener("mouseover", changeColor);
+}
+
+function changeColor(e){
+    console.log(e.target);
+    e.target.style.background = generateRGBColor();
+}
+
+function generateRGBColor(){
+    let r,g,b, rgbColor;
+    r =  Math.floor(Math.random() * 256);
+    g =  Math.floor(Math.random() * 256);
+    b =  Math.floor(Math.random() * 256);
+    rgbColor = `rgb(${r}, ${g}, ${b})`;
+    return rgbColor
+}
+
+for(i=0; i< 16; i++){
+    createDiv();
+}
+
